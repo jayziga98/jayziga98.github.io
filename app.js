385571de0adfeccbaf259941a70f50e9e7d1b88a
Kozlitin Maxim <zheka.petrov.99@list.ru>
2022-09-10 17:05:39 +0300

    telegram-web-app.js script connected.

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let item = document.getElementById("item");
-item.style.background = "blue";
-
-item.addEventListener("click", function(){
-    item.style.background = "black";
-});
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
